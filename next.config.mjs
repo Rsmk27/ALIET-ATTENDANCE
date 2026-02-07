1110748f77daea88f5b41c5673365a03ce6a57d4
ramunarlapati-13 <ramunarlapati@gmail.com>
2026-02-08 00:59:33 +0530

    pre-deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    basePath: '/ALIETake',
     images: {
+        unoptimized: true,
         domains: ['firebasestorage.googleapis.com'],
     },
 };
